75ac2eae08eb401ec83b48857170941f01d834df
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2024-02-02 13:10:14 -0500

    ,m

---
+++ vite-project/src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+<h1 v-if="answer">Correct</h1>
+<h2 v-else>Incorrect</h2>
+<h2 v-for="animal in animals" :key="animal">{{ animal }}</h2>
+  </div>
+</template>
+
+<script setup>
+const answer = true;
+const animals =["Ape", "Baboon", "elk", "Joer Mother"]
+</script>
+
+<style scoped>
+h1{
+  background-color:red;
+}
+</style>
